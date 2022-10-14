75f6679f7868987cd8eadde9ce750042c33f4e63
Muntasir Munna <muntasir.munna@metro-markets.de>
2022-10-14 12:48:39 +0200

    Remove unnecessary code blocks, use routes.yaml file instead of route annotations, update deprecated paths and db access

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,15 +11,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/',
-        name: 'root',
-        methods: ['GET'],
-    )]
     public function index(ArticleRepository $articleRepository): Response
     {
         $data = [];
@@ -28,10 +22,31 @@ class ArticleController extends AbstractController
         return $this->render('articles/index.html.twig', $data);
     }
 
-    #[Route('/article/view/{id}',
-        name: 'viewArticle',
-        methods: ['GET'],
-    )]
+    public function createArticle(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('body', TextareaType::class, ['attr' => ['class' => 'form-control']])
+            ->add('save', SubmitType::class, ['label' => 'Create Article', 'attr' => ['class' => 'form-control btn btn-primary mt-3'],])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $articleManager = $doctrine->getManager();
+            $articleManager->persist($article);
+            $articleManager->flush();
+            return $this->redirectToRoute('viewArticle', ['id' => $article->getId()]);
+        }
+
+        return $this->renderForm('articles/articleForm.html.twig', [
+            'form' => $form,
+            'title' => 'Create a new Article!',
+        ]);
+    }
+
     public function viewArticle(ArticleRepository $articleRepository, int $id): Response
     {
         $data = [];
@@ -44,29 +59,6 @@ class ArticleController extends AbstractController
 
     }
 
-    #[Route('/article/delete/{id}',
-        name: 'deleteArticle',
-        methods: ['GET'],
-    )]
-    public function deleteArticle(ArticleRepository $articleRepository, ManagerRegistry $doctrine, int $id): Response
-    {
-        $article = $articleRepository->find($id);
-        if(!$article) {
-            return $this->render('articles/invalid.html.twig');
-        } else {
-//            $articleManager = $doctrine->getManager();
-//            $articleManager->remove($article);
-//            $articleManager->flush();
-            $articleRepository->remove($article, true);
-            return $this->redirectToRoute('root');
-        }
-
-    }
-
-    #[Route('/article/update/{id}',
-        name: 'updateArticle',
-        methods: ['GET', 'POST'],
-    )]
     public function updateArticle(ArticleRepository $articleRepository, ManagerRegistry $doctrine, Request $request, $id): Response
     {
         $article = $articleRepository->find($id);
@@ -91,32 +83,15 @@ class ArticleController extends AbstractController
         ]);
     }
 
-    #[Route('/article/create',
-        name: 'createArticle',
-        methods: ['GET', 'POST'],
-    )]
-    public function createArticle(ManagerRegistry $doctrine, ValidatorInterface $validator, Request $request): Response
+    public function deleteArticle(ArticleRepository $articleRepository, int $id): Response
     {
-        $article = new Article();
-        $form = $this->createFormBuilder($article)
-            ->add('title', TextType::class, ['attr' => ['class' => 'form-control']])
-            ->add('body', TextareaType::class, ['attr' => ['class' => 'form-control']])
-            ->add('save', SubmitType::class, ['label' => 'Create Article', 'attr' => ['class' => 'form-control btn btn-primary mt-3'],])
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()) {
-            $article = $form->getData();
-            $articleManager = $doctrine->getManager();
-            $articleManager->persist($article);
-            $articleManager->flush();
-            return $this->redirectToRoute('viewArticle', ['id' => $article->getId()]);
+        $article = $articleRepository->find($id);
+        if(!$article) {
+            return $this->render('articles/invalid.html.twig');
+        } else {
+            $articleRepository->remove($article, true);
+            return $this->redirectToRoute('root');
         }
 
-        return $this->renderForm('articles/articleForm.html.twig', [
-            'form' => $form,
-            'title' => 'Create a new Article!',
-        ]);
     }
 }
